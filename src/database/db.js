cdc5fa9baacb77aa5472e1be4ea45534fa32b807
Eusébio Salomão <eusebiosalomao@outlook.com>
2023-08-12 17:03:48 +0100

    Conex e config vercel

--- src/database/db.js
+++ src/database/db.js
@@ -3,8 +3,9 @@ const connectDB = () => {
     console.log("Aguardando a conexão com o Mongodb Atla...")
 
     mongoose.Promise = global.Promise;
-    mongoose.connect("mongodb+srv://alomao:terebango@cluster0.rmvzzy3.mongodb.net/?retryWrites=true&w=majority").then(() => {
-        console.log("BD SS Mongo Atlas conectado em online!")
+    
+    mongoose.connect("mongodb+srv://salomao:terebango12@cluster0.jbkqm7r.mongodb.net/?retryWrites=true&w=majority").then(() => {
+        console.log("BD LRDD-290-CUITO Mongo Atlas conectado em online!")
     }).catch((erro) => {
         console.log("Erro de Conexão com Mongo Atlas: " + erro)
         //mongoose.connect("mongodb://localhost/BD_SMedia")
