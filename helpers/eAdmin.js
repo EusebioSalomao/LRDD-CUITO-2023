b978bfc65f55488e5320ac9843b15b544841ec4d
Eusébio Salomão <eusebiosalomao@outlook.com>
2023-08-21 17:10:39 +0100

    VerifyLogin

--- helpers/eAdmin.js
+++ helpers/eAdmin.js
@@ -1,4 +1,8 @@
 //Para restringir acesso
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
+import { findUserByIdService } from '../src/services/user.service.js';
+dotenv.config()
 
 export const eAdmin = (req, res, next) => {
     if (req.isAuthenticated() && req.user.eAdmin == 1) {
@@ -30,9 +34,34 @@ export const eFinanc = (req, res, next) => {
 }
 
 export const veryLogin = async (req, res, next) => {
-    if ( await req.isAuthenticated()) {
-        return next();
+    try {
+        
+        const authorization = 'Bear eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0ZDc3Y2RjMGQxYTNmNzc3NTJlMDUwZSIsImlhdCI6MTY5MjYzMzM0OCwiZXhwIjoxNjkyNzE5NzQ4fQ.V8Rc82QWm3k1vhAlICSCbDUz39GbuuVd8PsIYSmCUSs'
+        if(!authorization){
+            return res.status(401).send('Acesso negado!')
+        }
+        const parts = authorization.split(" ")
+        if(parts.length !== 2){
+            return res.status(401).send('Acesso negado! <br> O tokem so pode ter duas palabras!: Bear Token')
+        }
+        const [schema, token] = parts;
+        if(schema !== 'Bear'){
+            return res.status(401).send('Acesso negado! <br> A primeira palavra deve ser Bear!: Bear Token')
+        }
+
+        //Validando o token com jesonwebtoken
+        jwt.verify(token, process.env.SECRET_JWT, async (erro, decoded) => {
+            if(erro){
+            return res.status(401).send('Acesso negado! <br> Token invalido')
+            }
+            const user = await findUserByIdService(decoded.id);
+            if(!user){
+            return res.status(401).send('Usuario não existente!')
+            }
+            req.userId = user._id;
+            return next()
+        })
+    } catch (error) {
+        res.status(500).send({message: error.message})
     }
-    req.flash('error_msg', 'Inicie sessão para aceder a área desejada!')
-    res.redirect('/user/login')
 }
